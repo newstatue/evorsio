0cad45660b1499699f883bdec2b05d809be7117e
newstatue <yixing040506@outlook.com>
2026-09-13 23:10:31 +0800

    调整样式属性以适应不同平台

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -5,26 +5,27 @@ import { TitleBar } from "@/components/layout/TitleBar.tsx"
 import { System } from "@wailsio/runtime"
 
 export const Route = createRootRoute({
-  component: RootComponent,
+    component: RootComponent,
 })
 
 function RootComponent() {
-  const isWindows = System.IsWindows()
+    const isWindows = System.IsWindows()
+    const isDesktop = System.IsDesktop()
 
-  const style = {
-    "--titlebar-height": isWindows ? "2rem" : "0px",
-    "--header-height": "3.75rem",
-    "--header-macos-pd": "5.5rem",
-    "--sidebar": "transparent",
-  } as CSSProperties
+    const style = {
+        "--titlebar-height": isWindows ? "2rem" : "0px",
+        "--header-height": "3.75rem",
+        "--header-macos-pd": "5.5rem",
+        ...(isDesktop ? { "--sidebar": "transparent" } : {}),
+    } as CSSProperties
 
-  return (
-    <div style={style} className={"flex h-svh flex-col overflow-hidden"}>
-      {isWindows && <TitleBar />}
-      <main className="min-h-0 min-w-0 flex-1 overflow-y-auto">
-        <Outlet />
-      </main>
-      <TanStackRouterDevtools position="bottom-right" />
-    </div>
-  )
+    return (
+        <div style={style} className={"flex h-svh flex-col overflow-hidden"}>
+            {isWindows && <TitleBar />}
+            <main className="min-h-0 min-w-0 flex-1 overflow-y-auto">
+                <Outlet />
+            </main>
+            <TanStackRouterDevtools position="bottom-right" />
+        </div>
+    )
 }
